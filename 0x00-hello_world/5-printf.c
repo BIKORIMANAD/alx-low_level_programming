6c8e53b17c19e4ebede3084effa0753a6e7fb581
BIKORIMANAD <46579613+BIKORIMANAD@users.noreply.github.com>
2023-02-17 00:30:26 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*main - prints the phrase "Programming is like building a multilingual puzzle
-*Return: 0 if exited properly, non-zero otherwise
-*/
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, ");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
