678b8c6717ff287890af2234998738b5a621921a
BIKORIMANAD <bikorimanadavid.rw@gmail.com>
2023-02-21 19:59:12 -0800

    function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result
+ * @one: first number input
+ * @two: second number input
+ * Return: a + b
+ */
+int add(int n, int b)
+{
+	return (one + two);
+}
